ea4bffede9291c5a0836734e7b0b2b08a462c4b4
Nima <nimamiri9248@gmail.com>
2023-03-18 17:26:34 +0330

    first steps of advanced blog

---
+++ core/accounts/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin 
+from .models import User ,Profile
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['email', 'is_staff', 'is_active']
+    list_filter=['email', 'is_staff', 'is_active']
+    search_fields = ['email']
+    ordering = ['email']
+    fieldsets=(
+        ("Authentication", {'fields':('email', 'password')}),
+        ('Permissions', {'fields':('is_staff', 'is_active' , 'is_superuser')}),
+        ('group permissions', {'fields':('groups', 'user_permissions')}),
+        ('dates', {'fields':('last_login',)}),
+    )
+    add_fieldsets = (
+        ("Authentication", {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2","is_staff",
+                "is_active", "is_superuser"
+            )}
+        ),
+    )
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
---
+++ core/blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+class Post(models.Model):
+    #This is a class to define posts for blog app
+    title = models.CharField(max_length=100)
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    image=models.ImageField(blank=True, null=True)
+    content = models.TextField()
+    status=models.BooleanField(default=False)
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+class Category(models.Model):
+    #This is a class to define categories for blog app
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
